8c188e74c8dc51af1fc5be2548ace9badacfbdeb
toastef <94532614+toastef@users.noreply.github.com>
2022-09-30 11:20:56 +0200

    upd

--- src/DataFixtures/CategoryFixture.php
+++ src/DataFixtures/CategoryFixture.php
@@ -2,15 +2,22 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixture extends Fixture
 {
+    private array $categories = ['PHP 8', 'Symfony', 'Laravel', 'Security', 'DataBase',
+        'JavaScript','Front End'];
     public function load(ObjectManager $manager): void // object manager sert a manipuler la base de donnée voir les méthodes injectée en dépendance
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        foreach($this->categories as $category) {
+            $cat = new Category();  // creation d'une nouvelle classe
+            $cat->setName($category); // setnamle des catégorie
+            $manager->persist($cat); // on persist la $cat
+        }
 
         $manager->flush();
     }
