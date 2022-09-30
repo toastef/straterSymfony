dc4cd543ef8685975c45686bf8174bd1f22c670a
toastef <94532614+toastef@users.noreply.github.com>
2022-09-30 14:53:16 +0200

    30-09-2022

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     #[Route('/posts', name: 'posts')]
-    public function posts(): Response
+    public function posts(PostRepository $repository): Response
     {
+        $posts = $repository->findBy(
+            ['isPublished'=>true], // premier array pour les criteres
+            ['title'=> 'ASC'], // deuxiemme tab
+            10
+        );
         return $this->render('post/posts.html.twig', [
-
+            'posts' => $posts,
         ]);
     }
 }
--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
-class PostFixtures extends Fixture
+class PostFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+       $faker = Faker\Factory::create();
+        $categories = $manager->getRepository(Category::class)->findAll(); // utilisation de la classe du manager getrepository qui va permettre de récup les donnée de l'antitée et le findall . va chercher toutes les infos et stocke les dans $categories
+        $countCat = count($categories);
+        for($i=1; $i<= 35 ; $i++)
+        {
+            $post = new Post();
+            $post->setTitle($faker->words(3,true))// générer un titre de 3 mots avec faker
+                ->setContent($faker->paragraphs(3,true))
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setImage($i.'.png')
+                ->setIsPublished($faker->boolean(90))
+                ->setCategory($categories [$faker->numberBetween(0,$countCat -1)]); // nombre aléatoire entre 0 et le nombre d"élément du tab -1 pour générer le numéro du tableau
+            $manager->persist($post);
 
+        }
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [CategoryFixture::class,
+            ];
     }
 }
